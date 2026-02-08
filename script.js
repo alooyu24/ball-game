df7dfc63a20dd0cf6bd3767d808fe7fcfd837100
Ali Hasan Salman Hasan <nassy02056@gmail.com>
2026-02-08 10:55:48 +0300

    crated the grid and the ball

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const board = document.querySelector(".board")
+
+const rows = 10
+const columns = 7
+
+for (let i = 0; i < rows * columns; i++) {
+  const cell = document.createElement("div")
+  cell.classList.add("cell")
+  board.appendChild(cell)
+}
+
+const cells = document.querySelectorAll(".cell")
+
+const ballColl = Math.floor(columns / 2)
+const ballRow = rows - 1
+const ballIndex = ballRow * columns + ballColl
+
+cells[ballIndex].classList.add("ball")
