84ca1d8cd0439bf0d8f65cf8d9487cd0ac53aeb0
Ali Hasan Salman Hasan <nassy02056@gmail.com>
2026-02-08 16:00:23 +0300

    added Arrowup and interval

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const board = document.querySelector(".board")
 
 const rows = 10
 const columns = 7
+let ball
 
 for (let i = 0; i < rows * columns; i++) {
   const cell = document.createElement("div")
@@ -11,8 +12,61 @@ for (let i = 0; i < rows * columns; i++) {
 
 const cells = document.querySelectorAll(".cell")
 
-const ballColl = Math.floor(columns / 2)
+let ballColl = Math.floor(columns / 2)
 const ballRow = rows - 1
-const ballIndex = ballRow * columns + ballColl
+let ballIndex = ballRow * columns + ballColl
 
 cells[ballIndex].classList.add("ball")
+
+const handleKeyPress = (event) => {
+  if (event.key === "ArrowLeft") {
+    moveBallLeft()
+  } else if (event.key === "ArrowRight") {
+    moveBallRight()
+  } else if (event.key === "ArrowUp") {
+    setInterval(() => {
+      moveUp()
+    }, 1000)
+  }
+}
+
+const moveBallLeft = () => {
+  if (ballColl === 0) return
+
+  ball = cells[ballIndex].classList.remove("ball")
+
+  ballColl--
+  ballIndex = ballRow * columns + ballColl
+
+  ball = cells[ballIndex].classList.add("ball")
+}
+
+const moveBallRight = () => {
+  if (ballColl === columns - 1) return
+
+  ball = cells[ballIndex].classList.remove("ball")
+
+  ballColl++
+  ballIndex = ballRow * columns + ballColl
+  console.log(ballIndex)
+
+  ball = cells[ballIndex].classList.add("ball")
+}
+
+const moveUp = () => {
+  ball = cells[ballIndex].classList.remove("ball")
+
+  ballColl -= 7
+  ballIndex = ballRow * columns + ballColl
+  console.log(ballIndex)
+  ball = cells[ballIndex].classList.add("ball")
+}
+
+document.addEventListener("keydown", handleKeyPress)
+
+const goalRow = 0
+let goalCol = Math.floor(columns / 2)
+let goalIndex = goalRow * columns + goalCol
+console.log(ballColl)
+
+cells[goalIndex].classList.add("goal")
